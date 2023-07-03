06c4aaac59edf58fde496a159efdcfd9d99f35e3
TomatoQt <a491122844@gmail.com>
2023-07-03 22:55:41 +0800

    Update

--- linkedList/LC24.java
+++ linkedList/LC24.java
@@ -2,6 +2,7 @@ package linkedList;
 
 // 两两交换链表中的节点
 public class LC24 {
+    // 递归
     public ListNode swapPairs(ListNode head) {
         if (head == null || head.next == null)
             return head;
@@ -12,5 +13,24 @@ public class LC24 {
         cur.next = newNode;
 
         return next;
+    }
+
+    public ListNode swapPairs_(ListNode head) {
+        ListNode dummyHead = new ListNode(-1, head);
+        ListNode cur = dummyHead;
+
+        while (cur.next != null && cur.next.next != null) {
+            ListNode firstNode = cur.next;
+            ListNode secondNode = firstNode.next;
+            ListNode temp = secondNode.next;
+            // swap
+            cur.next = secondNode;
+            secondNode.next = firstNode;
+            firstNode.next = temp;
+            // next round
+            cur = firstNode;
+        }
+
+        return dummyHead.next;
     }
 }
