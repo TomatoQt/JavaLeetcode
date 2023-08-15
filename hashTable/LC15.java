5502b879adb81f76a6890a83a0301a45c9182dd4
TomatoQt <a491122844@gmail.com>
2023-08-15 19:27:34 +0800

    Update 15

--- hashTable/LC15.java
+++ hashTable/LC15.java
@@ -7,32 +7,37 @@ import java.util.List;
 // 双指针
 public class LC15 {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>(); // sorted
+        List<List<Integer>> result = new ArrayList<>(); // 存结果
+        Arrays.sort(nums); // 先排序
 
-        // smallest > 0 || biggest < 0
+        // 升序数组，最小元素大于0或者最大元素小于0，都没有必要再找了
         if (nums[0] > 0 || nums[nums.length - 1] < 0)
-            return ans;
-        for (int i = 0; i < nums.length; i++) {
-            int left = i + 1, right = nums.length - 1;
+            return result;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // 去重
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
+            int left = i + 1, right = nums.length - 1;
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
-                if (sum > 0) {
-                    right--;
-                } else if (sum < 0) {
+                if (sum < 0) {
                     left++;
+                } else if (sum > 0) {
+                    right--;
                 } else {
-                    ans.add(new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right])));
+                    result.add(new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right])));
+                    // 去重
                     while (left < right && nums[left] == nums[left + 1]) left++;
                     while (left < right && nums[right] == nums[right - 1]) right--;
-                    right--;
+
+                    // 找到答案，双指针同时变化
                     left++;
+                    right--;
                 }
             }
         }
 
-        return ans;
+        return result;
     }
 }
