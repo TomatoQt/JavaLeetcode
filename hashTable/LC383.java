baf3b5834952fa1851cf151acf39292d15a34669
TomatoQt <a491122844@gmail.com>
2023-08-15 18:22:47 +0800

    Update 383

--- hashTable/LC383.java
+++ hashTable/LC383.java
@@ -5,27 +5,21 @@ import java.util.Map;
 
 public class LC383 {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> charCount = new HashMap<>();
+        // 记录26个字母的出现次数
+        int[] alphabet = new int[26];
 
+        // 统计可用字母数量
         for (int i = 0; i < magazine.length(); i++) {
             char item = magazine.charAt(i);
-            // haven't put in
-            if (!charCount.containsKey(item)) {
-                charCount.put(item, 1);
-            } else { // +1 based on previous value
-                int itemCount = charCount.get(item);
-                charCount.put(item, ++itemCount);
-            }
+            alphabet[item - 'a']++;
         }
 
+        // 消耗字母
         for (int i = 0; i < ransomNote.length(); i++) {
-            char item  = ransomNote.charAt(i);
-            if (!charCount.containsKey(item) || charCount.get(item) == 0) {
+            char item = ransomNote.charAt(i);
+            // 不够用，false
+            if (--alphabet[item - 'a'] < 0)
                 return false;
-            } else {
-                int itemCount = charCount.get(item);
-                charCount.put(item, --itemCount);
-            }
         }
 
         return true;
