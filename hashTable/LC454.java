c6642259664fa3f4ade46e3661dd7b9f7b5ebcc5
TomatoQt <a491122844@gmail.com>
2023-08-15 18:13:53 +0800

    Update 454

--- hashTable/LC454.java
+++ hashTable/LC454.java
@@ -5,30 +5,24 @@ import java.util.Map;
 
 public class LC454 {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
-        Map<Integer, Integer> sum_times = new HashMap<>(); // record nums1, nums2
-        int count = 0;
+        // 记录A、B中元素之和+出现次数
+        Map<Integer, Integer> ABhash = new HashMap<>();
+        int targetCount = 0;
 
-        for (int item1 : nums1) {
-            for (int item2 : nums2) {
-                int sum_temp = item1 + item2;
-                if (sum_times.containsKey(sum_temp)) {
-                    int times_temp = sum_times.get(sum_temp);
-                    sum_times.put(sum_temp, times_temp + 1);
-                } else {
-                    sum_times.put(sum_temp, 1);
-                }
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                // 取出“和”+1，统计“和”出现次数
+                ABhash.put(nums1[i] + nums2[j], ABhash.getOrDefault(nums1[i] + nums2[j], 0) + 1);
             }
         }
 
-        for (int item3 : nums3) {
-            for (int item4 : nums4) {
-                int sum_temp = item3 + item4;
-                if (sum_times.containsKey(-sum_temp)) {
-                    count += sum_times.get(-sum_temp);
-                }
+        for (int i = 0; i < nums3.length; i++) {
+            for (int j = 0; j < nums4.length; j++) {
+                if (ABhash.containsKey(-nums3[i] - nums4[j]))
+                    targetCount += ABhash.get(-nums3[i] - nums4[j]);
             }
         }
 
-        return count;
+        return targetCount;
     }
 }
