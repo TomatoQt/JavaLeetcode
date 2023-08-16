6ccae5daba055886e0a77722e68fccf78e56666b
TomatoQt <a491122844@gmail.com>
2023-08-16 19:49:30 +0800

    Update 151

--- Main.java
+++ Main.java
@@ -1,32 +1,6 @@
-import java.net.Inet4Address;
 import java.util.*;
-
-import linkedList.ListNode;
+import java.util.stream.Collectors;
 
 public class Main {
-    public ListNode detectCycle(ListNode head) {
-        ListNode slow = head, fast = head; // 初始化 speed(fast) = 2 * speed(slow)
 
-        // 第一次循环，寻找相遇点
-        do {
-            if (fast == null)
-                return null;
-            slow = slow.next;
-            fast = fast.next.next;
-        } while (slow != fast);
-
-        // 推论：2个节点，一个从开头出发，一个从meet出发，以相同速度前进，再相遇就是环入口
-        // 重新利用fast
-        fast = head;
-        do {
-            slow = slow.next;
-            fast = fast.next;
-        } while (slow != fast);
-
-        return slow;
-    }
-
-    public static void main(String[] args) {
-
-    }
 }
--- StringProcess/LC151.java
+++ StringProcess/LC151.java
@@ -3,20 +3,57 @@ package StringProcess;
 // 正则表达式貌似不处理字符串的前导和后置的匹配部分
 public class LC151 {
     public String reverseWords(String s) {
-        s = s.trim(); // remove front end spaces
-        String[] split = s.split("\\s+");// spaces
+        // 去除首尾及中间的多余空格
+        StringBuilder sb = removeSpaces(s);
 
-        // reverse
-        int left = 0, right = split.length - 1;
+        // 反转整个字符串
+        reverseString(sb, 0, sb.length() - 1);
+
+        // 反转单个单词
+        reverseEachWord(sb);
+
+        return sb.toString();
+    }
+
+    private StringBuilder removeSpaces(String str) {
+        int left = 0, right = str.length() - 1;
+        StringBuilder res = new StringBuilder();
+
+        // 去除首尾多余空格
+        while (str.charAt(left) == ' ') left++;
+        while (str.charAt(right) == ' ') right--;
+
+        while (left <= right) {
+            // 遇到非空格就append，遇到空格了，只保留已经在res中的那个
+            if (str.charAt(left) != ' ' || res.charAt(res.length() - 1) != ' ')
+                res.append(str.charAt(left));
+            left++;
+        }
+
+        return res;
+    }
+
+    private void reverseString(StringBuilder sb, int left, int right) {
         while (left < right) {
-            String temp = split[left];
-            split[left] = split[right];
-            split[right] = temp;
+            char temp = sb.charAt(left);
+            sb.setCharAt(left, sb.charAt(right));
+            sb.setCharAt(right, temp);
+
             left++;
             right--;
         }
+    }
 
-        // join
-        return String.join(" ", split);
+    private void reverseEachWord(StringBuilder sb) {
+        int left = 0, right = left + 1;
+
+        while (right < sb.length()) {
+            // 找到要反转的区间[left, right - 1]
+            while (right < sb.length() && sb.charAt(right) != ' ') right++;
+            reverseString(sb, left, right - 1);
+
+            left = right + 1;
+            right = left + 1;
+        }
     }
 }
