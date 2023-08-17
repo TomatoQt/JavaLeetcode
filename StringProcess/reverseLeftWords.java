e7a1eb56dc7c0b581ebd57e744b51650b51b7d56
TomatoQt <a491122844@gmail.com>
2023-08-17 16:04:47 +0800

    Update

--- StringProcess/reverseLeftWords.java
+++ StringProcess/reverseLeftWords.java
@@ -2,9 +2,11 @@ package StringProcess;
 
 public class reverseLeftWords {
     public String reverseLeftWords(String s, int n) {
-        StringBuilder sb = new StringBuilder(s.substring(n));
+        StringBuilder sb = new StringBuilder(s);
         for (int i = 0; i < n; i++) {
-            sb.append(s.charAt(i));
+            char c = sb.charAt(0);
+            sb.deleteCharAt(0);
+            sb.append(c);
         }
 
         return sb.toString();
