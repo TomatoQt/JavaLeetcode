0477e58c18384805020a17926d05a8619b4f94d5
TomatoQt <a491122844@gmail.com>
2023-08-20 22:02:51 +0800

    Update 347

--- Stack_Queue/LC347.java
+++ Stack_Queue/LC347.java
@@ -12,18 +12,21 @@ public class LC347 {
      *                                从队头到队尾按从大到小排就是最大堆（大顶堆）--->队头元素相当于堆的根节点
      * */
     public int[] topKFrequent(int[] nums, int k) {
+        int[] result = new int[k];
+        // (number, frequency)
         Map<Integer, Integer> map = new HashMap<>();
-        for (int num : nums)
-            //key为数组元素值,val为对应出现次数
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        // 统计频率
+        for (int item : nums)
+            map.put(item, map.getOrDefault(item, 0) + 1);
 
-        //在优先队列中存储二元组(num,cnt),cnt表示元素值num在数组中的出现次数
-        //出现次数按从队头到队尾的顺序是从小到大排,出现次数最低的在队头(相当于小顶堆)
         PriorityQueue<int[]> pq = new PriorityQueue<>((pair1, pair2) -> pair1[1] - pair2[1]);
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
             if (pq.size() < k) {
                 pq.add(new int[]{entry.getKey(), entry.getValue()});
-            } else {
+            } else { // pq.size() >= k
+                // 只有当前频率大于小顶堆频率时才弹出，考虑以下例子：
+                // k=2, 频率数组=[3,2,1]
+                // 不能为了1，把3弹出
                 if (entry.getValue() > pq.peek()[1]) {
                     pq.poll();
                     pq.add(new int[]{entry.getKey(), entry.getValue()});
@@ -31,11 +34,10 @@ public class LC347 {
             }
         }
 
-        int[] ans = new int[k];
-        for (int i = k - 1; i >= 0; i--) {
-            ans[i] = pq.poll()[0];
-        }
+        int index = 0;
+        while (pq.size() > 0)
+            result[index++] = pq.poll()[0];
 
-        return ans;
+        return result;
     }
 }
