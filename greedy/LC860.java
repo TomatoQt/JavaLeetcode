07988164cfe6543a9417a9d5c778ce3a45e8f4ba
TomatoQt <a491122844@gmail.com>
2023-09-18 16:54:23 +0800

    update 860

--- greedy/LC860.java
+++ greedy/LC860.java
@@ -1,7 +1,31 @@
 package greedy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LC860 {
     public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
 
+        for (int i = 0; i < bills.length; i++) {
+            int money = bills[i];
+            if (money == 5)
+                five++;
+            else if (money == 10) {
+                five--;
+                ten++;
+            } else if (money == 20) {
+                if (ten > 0) {
+                    ten--;
+                    five--;
+                } else {
+                    five -= 3;
+                }
+            }
+            if (five < 0 || ten < 0)
+                return false;
+        }
+        return true;
     }
 }
